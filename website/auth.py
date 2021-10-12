5b6339b47dd1244c8f339545e6e807e3a670e00c
pierluigi <pierlucolombo@gmail.com>
2021-10-12 12:24:27 +0200

    backend for sign up post call

--- website/auth.py
+++ website/auth.py
@@ -3,16 +3,17 @@ views for auth
 '''
 
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('login')
+@auth.route('login', methods=['GET', 'POST'])
 def login():
     '''
     function that handles the login
     '''
+    data = request
     return render_template("login.html")
 
 
@@ -24,9 +25,28 @@ def logout():
     return render_template("logout.html")
 
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
     '''
     function that handles the sign up
     '''
+    if request.method == 'POST':
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        # input check
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')  
+        elif len(firstName) < 2:
+            flash('Email must be greater than 1 characters', category='error')  
+        elif password1 != password2:
+            flash("password doesn't match", category='error') 
+        elif len(password1) < 7:
+            flash("password too short, lenght must be at least 7 characters", category='error') 
+        else:
+            # add user
+            flash("user created", category='success') 
+
     return render_template("sign_up.html")
