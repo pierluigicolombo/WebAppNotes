0bdaa42e1bce03a708954f055d16b5743ca42afe
pierluigi <pierlucolombo@gmail.com>
2021-10-13 17:13:07 +0200

    configured db

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,10 @@ create the app flask
 from flask import Flask
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
@@ -15,6 +19,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'WQQXQxwecwedwedwxw'  # ENCRYPT AND SECURE THE
     # COOKIE AND SESSION DATA
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
